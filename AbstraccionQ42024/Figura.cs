93c3af135370fdd623b062d80f57046709d636be
gurdian0614 <ivan.gurdian@outlook.com>
2024-10-18 18:49:08 -0600

    Creando clase abstracta

---
+++ AbstraccionQ42024/Figura.cs
@@ -0,0 +1,14 @@
+﻿
+namespace AbstraccionQ42024
+{
+    public abstract class Figura
+    {
+        public abstract double Area();
+        public abstract double Perimetro();
+        public void Imprimir()
+        {
+            Console.WriteLine($"Area: {Area()}");
+            Console.WriteLine($"Perímetro: {Perimetro()}");
+        }
+    }
+}
